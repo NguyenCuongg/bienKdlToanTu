f5c1d70e4618e5b9584d146a75c04f6d391f2669
- <->
2020-09-07 16:42:44 +0700

    bt

---
+++ toanTu.js
@@ -0,0 +1,9 @@
+let inputwidth ;
+let inputheight ;
+inputwidth = prompt("nhap vao width: ");
+inputheight = prompt("nhap vao height: ");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area ;
+area = (width * height);
+alert('this arae is: '+ area);
